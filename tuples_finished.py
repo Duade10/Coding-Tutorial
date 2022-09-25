1244dc285ee18cfade1a8105ec005b88ac76e5a6
Otunla Timilehin <otunlaisaac8@gmail.com>
2022-09-25 03:49:17 -0700

    create tuples_finished.py

---
+++ tuples_finished.py
@@ -0,0 +1,18 @@
+# DATA TYPES
+
+# 1. Data Types
+# Data Types Examples: Strings, Integer, Boolean, List.
+
+# TUPLE
+
+# TUPLES ARE LISTS THAT ARE IMMUTABLE
+
+# When you create you cannot change any of it's content
+# The contents of tuples are always in between brackets ()
+
+# LIST
+# utensils = ['spoon', 'plates', 'forks']
+# TUPLE
+utensils = ('spoon', 'plates', 'forks')
+print(utensils[0])
+# Methods like pop(), append(), remove() etc are not allowed when using a TUPLE, this is because these methods change the contents of the TUPLE
