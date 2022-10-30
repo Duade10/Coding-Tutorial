d2d19c4a72bcbe00abeeb9e275a07d61ce94d810
Otunla Timilehin <otunlaisaac8@gmail.com>
2022-10-30 14:40:12 -0700

    Update baseball.py

--- baseball/baseball.py
+++ baseball/baseball.py
@@ -2,13 +2,9 @@ from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__) # Double Underscore (__)
 
-@app.route('/')
-def hello():
-    return '<h1> Hello, World! I am here</h1>'
-
 result = []
 output = []
-@app.route('/get-input/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def user_form():
     user_input = request.form.get('text', None)
     alert = None
@@ -53,37 +49,4 @@ def user_form():
 @app.route('/clear-input/')
 def clear_input():
     result.clear()
-    return redirect('/get-input/')
-
-# result = []
-# @app.route('/get-input/', methods=["POST",])
-# def get_input():
-#     user_input = request.form['text']
-#     if len(result) > 1:
-#         if (result[0], result[1]) is int:
-#             if user_input == "+":
-#                 x = result[-1] + result[-2]
-#                 result.append(x)
-#             elif user_input == "D":
-#                 x = result[-1]*2
-#                 result.append(x)
-#             elif user_input == "C":
-#                 result.pop()
-#             else:
-#                 result.append(int(user_input))
-#     else:
-#         if user_input == "+":
-#             x = result[-1] + result[-2]
-#             result.append(x)
-#         elif user_input == "D":
-#             x = result[-1]*2
-#             result.append(x)
-#         elif user_input == "C":
-#             result.pop()
-#         else:
-#             result.append(int(user_input))
-#             return f"<h1> Your input is {user_input} Do not input C or D as either of the first two input</h1>"
-
-
-
-
+    return redirect('/')
